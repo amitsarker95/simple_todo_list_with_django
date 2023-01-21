e07e615db33c9556dc748cab851c0064f21a5071
Hp <amitsarker95@gmail.com>
2023-01-22 00:59:53 +0600

    practise

---
+++ Todo_list/todo_list/base/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . views import TaskList
+from . views import TaskDetail
+from . views import TaskCreate
+from . views import TaskUpdate
+from . views import TaskDelete, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    
+    path('login/',CustomLoginView.as_view(), name='login'),
+    path('logout/',LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
+    path('',TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(), name='task'),
+    path('task-create/',TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/',TaskDelete.as_view(), name='task-delete'),
+    
+]
